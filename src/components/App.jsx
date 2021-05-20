d9f19644d525f7aa5508f755996b2981275053eb
hiro <hmaeyama0510@gmail.com>
2021-05-20 20:08:01 +0900

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [exlamation, setExclamation] = useState("");
+
+  function inputFName(event) {
+    setFName(event.target.value);
+  }
+
+  function inputLName(event) {
+    setLName(event.target.value);
+  }
+
+  function handleChange(event) {
+    setExclamation("!");
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>
+        Hello {fName} {lName} {exlamation}
+      </h1>
+      <form onSubmit={handleChange}>
+        <input onInput={inputFName} name="fName" placeholder="First Name" />
+        <input onInput={inputLName} name="lName" placeholder="Last Name" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
